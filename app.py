3af393632e70aa66369c3be7ced86e729ee846b4
elaisemsunny <66775126+elaisemsunny@users.noreply.github.com>
2021-02-19 22:10:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
