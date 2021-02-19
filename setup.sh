6ee9b1c5dea66790c3a8cfdcdac85c3e57b9907d
elaisemsunny <66775126+elaisemsunny@users.noreply.github.com>
2021-02-19 18:40:51 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 mkdir -p ~/.streamlit/
 echo "\
-
+[general]\n\
+email = \"elaisemsunnyy@gmail.com\"\n\
+" > ~/.streamlit/Elaise@123.toml
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
